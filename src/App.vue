96c76b381f1cefc3efac0eb9c193fbe80abad492
JinPing <1641525429@qq.com>
2018-02-02 05:10:32 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    Hello Word
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/styuls">
+@import "~common/stylus/variable"
+
+#app {
+  color: $color-theme;
+}
+</style>
