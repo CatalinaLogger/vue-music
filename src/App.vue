b5b5715302147e928ef5b577f4fc3d7df6711333
JinPing <1641525429@qq.com>
2018-02-04 03:55:43 +0800

    页面骨架搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    Hello Word
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
 
+export default {
+  components: {
+    MHeader,
+    Tab
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/styuls">
-@import "~common/stylus/variable"
 
-#app {
-  color: $color-theme;
-}
 </style>
