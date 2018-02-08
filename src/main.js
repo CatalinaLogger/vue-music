a8c40ecc1715a5bb915c69cd4f576c3f2a586cc5
JinPing <1641525429@qq.com>
2018-02-09 07:12:21 +0800

    配置vuex完成

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import VueLazyload from 'vue-lazyload'
 import fastclick from 'fastclick'
 import App from './App'
 import router from './router'
+import store from './store'
 
 import 'common/stylus/index.styl'
 
@@ -17,5 +18,6 @@ Vue.use(VueLazyload, {
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
